554356c01d70844bc00afb4d44925d2c4be6860a
Ashish <ashish74624@gmail.com>
2023-05-15 23:45:32 +0530

    Responsive Poster Added

--- app/components/Newspaper.tsx
+++ app/components/Newspaper.tsx
@@ -4,8 +4,8 @@ import Image from "next/image"
 
 export default function Newspaper() {
   return (
-    <div className="lg:pr-32  w-[35vw] shadow-lg rounded-l-full pl-20 shadow-yellow-600 ">
-      <Image src={'/newsppr.png'} alt='Newspaper' width={400} height={64} loading="lazy" onLoad={()=>{console.log("Image")}} />
+    <div className="lg:pr-32 w-0 md:h-min h-min md:w-[40vw] lg:w-[35vw] shadow-lg rounded-l-full pl-20 shadow-yellow-600 ">
+      <Image src={'/newsppr.png'} alt='Newspaper' width={400} height={100} loading="lazy" onLoad={()=>{console.log("Image")}} />
     </div>
   )
 }
--- app/components/Poster.tsx
+++ app/components/Poster.tsx
@@ -14,18 +14,19 @@ export default function Poster() {
   return (
     <>
       <section
-        className={`relative bg-[#F5C347] lg:h-[62vh] w-screen flex items-center justify-between lg:pl-32 ${merriweather.className} overfolw-clip z-0 `}>
-        <div className=' space-y-10'>
-            <p className='text-black text-8xl'>Stay curious</p>
+        className={`relative bg-transparent md:bg-[#F5C347] h-max  md:h-[45vh] lg:h-[56vh] w-screen flex flex-col md:flex-row items-center md:justify-between md:pl-14 md:mt-[5vh] mt-[6vh] lg:mt-[4vh] lg:pl-32 ${merriweather.className} overfolw-clip z-0 `}>
+        <div className='pt-4 md:pb-0 pb-7 md:pt-0 space-y-8 md:space-y-10 md:px-0 px-4 md:bg-transparent md:w-max w-screen  bg-[#F5C347]'>
+            <p className='text-black text-7xl lg:text-[80px] md:text-6xl '>Stay curious</p>
             <div>
-            <p className='text-black text-xl'>Discover stories, thinking and expertise</p>
-            <p className='text-black text-xl'>from writers on any topic</p>
+            <p className='text-black text-3xl md:text-xl '>Discover stories, thinking and expertise</p>
+            <p className='text-black text-3xl md:text-xl '>from writers on any topic</p>
             </div>
             
-            <button className='bg-black lg:w-48 h-max lg:px-4 lg:py-2 hover:bg-white text-white hover:text-black transition duration-300 ease-in rounded-full'>
+            
+            <button className='bg-black w-max lg:w-48 h-max px-4 py-2 hover:bg-white text-white hover:text-black transition duration-300 ease-in rounded-full'>
             <Link href={`/Categories`}>Start Reading</Link></button>
         </div>
-        <div className='relative top-4'>
+        <div className='relative top-4 h-0 md:h-min bg-transparent '>
         <Newspaper/>
         </div>
       </section>
