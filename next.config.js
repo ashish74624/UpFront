4fa90522d15bfcb8abbfbade0f9bd28b4b8267b6
Ashish <ashish74624@gmail.com>
2023-05-16 19:52:03 +0530

    output: 'export' Added

--- next.config.js
+++ next.config.js
@@ -3,14 +3,7 @@ const nextConfig = {
     images: {
         domains: ['live-production.wcms.abc-cdn.net.au'],
       },
-      async generateStaticPaths() {
-        return {
-          '/': { app: '/' },
-          '/categories': { app: '/categories' },
-          '/:title': { app: '/[title]' },
-        };
-      },
-      exportTrailingSlash: true, 
+      output: 'export', 
 }
 
 module.exports = nextConfig
