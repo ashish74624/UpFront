4e95ea471385cb01dafd1e0f8a6c9f724567d8da
Ashish <ashish74624@gmail.com>
2023-05-17 11:25:15 +0530

    Replacing the .txt that keeps apearing

--- lib/getNews.ts
+++ lib/getNews.ts
@@ -1,6 +1,7 @@
 export default async function getNews(category:string){
-    const key = 'bc87faed5165437e98a35cf5993205eb'
-    const req = await fetch(`http://newsapi.org/v2/everything?q=${category}&apiKey=`+key,{
+    const key = 'bc87faed5165437e98a35cf5993205eb';
+    let category2 = category.replace(/\.txt$/, "");
+    const req = await fetch(`http://newsapi.org/v2/everything?q=${category2}&apiKey=`+key,{
         next:{
             revalidate:4
         }
