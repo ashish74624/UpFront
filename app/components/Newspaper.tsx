96c9389cdbaa7b978b21149a6a872320b7a5c464
Ashish <ashish74624@gmail.com>
2023-05-25 13:17:19 +0530

    Added Dark Mode to the Poster

--- app/components/Newspaper.tsx
+++ app/components/Newspaper.tsx
@@ -4,7 +4,7 @@ import Image from "next/image"
 
 export default function Newspaper() {
   return (
-    <div className="lg:pr-32 w-0 md:h-min h-min md:w-[40vw] lg:w-[35vw] shadow-lg rounded-l-full pl-20 shadow-yellow-600 ">
+    <div className="lg:pr-32 w-0 md:h-min h-min md:w-[40vw] lg:w-[35vw] shadow-lg rounded-l-full pl-20 shadow-yellow-600 dark:shadow-yellow-400">
       <Image src={'/newsppr.png'} alt='Newspaper' width={400} height={100} loading="lazy" onLoad={()=>{console.log("Image")}} />
     </div>
   )
--- app/components/Poster.tsx
+++ app/components/Poster.tsx
@@ -14,8 +14,8 @@ export default function Poster() {
   return (
     <>
       <section
-        className={`relative bg-transparent md:bg-[#F5C347] h-max  md:h-[45vh] lg:h-[56vh] w-screen flex flex-col md:flex-row items-center md:justify-between md:pl-14 md:mt-[5vh] mt-[6vh] lg:mt-[4vh] lg:pl-32 ${merriweather.className} overfolw-clip z-0 `}>
-        <div className='pt-4 md:pb-0 pb-7 md:pt-0 space-y-8 md:space-y-10 md:px-0 px-4 md:bg-transparent md:w-max w-screen  bg-[#F5C347]'>
+        className={`relative bg-transparent md:bg-[#F5C347] md:dark:bg-yellow-600  h-max  md:h-[45vh] lg:h-[56vh] w-screen flex flex-col md:flex-row items-center md:justify-between md:pl-14 md:mt-[5vh] mt-[6vh] lg:mt-[4vh] lg:pl-32 ${merriweather.className} overfolw-clip z-0 `}>
+        <div className='pt-4 md:pb-0 pb-7 md:pt-0 space-y-8 md:space-y-10 md:px-0 px-4 md:bg-transparent md:w-max w-screen  bg-[#F5C347] dark:bg-yellow-600'>
             <p className='text-black text-7xl lg:text-[80px] md:text-6xl '>Stay curious</p>
             <div>
             <p className='text-black text-3xl md:text-xl '>Discover stories, thinking and expertise</p>
